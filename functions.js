017066f977fc49d2cdcf4ff93500ee8816a992c1
everctba <everson777@gmail.com>
2025-12-05 21:55:19 -0300

    Add candle generation and display functions
    Introduces geraCandleAleatorio and printaArrayCandle functions to generate and display random OHLC candle data. Updates index.html to use these functions for creating and printing a set of candles.

--- functions.js
+++ functions.js
@@ -13,3 +13,32 @@ function geraValorAleatorio() {
    return preco;
 }
 
+function geraCandleAleatorio(tamanho, matriz) {
+   let i, j;
+
+   for (i = 0; i < tamanho; i++) {
+      matriz[i] = [];
+      for (j = 0; j < 4; j++) {
+         matriz[i][j] = geraValorAleatorio();
+         console.log(matriz[i][j]);
+         //abertura, fechamento, minima, maxima,
+      }
+   }
+   console.log("Linhas:", matriz.length);
+   console.log("Colunas:", matriz[0].length);
+   return matriz;
+}
+
+function printaArrayCandle(listaCandles) {
+   let i, j;
+
+   for (i = 0; i < listaCandles.length; i++) {
+      for (j = 0; j < 4; j++) {
+         document.write(listaCandles[i][j] + " ");
+
+
+      }
+      document.write("<br>");
+
+   }
+}
